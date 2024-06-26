8506043b25a1450ee9699ce47e28db89cc604e41
Abduweli Ali <149704850+Localabduweli@users.noreply.github.com>
2024-06-26 16:45:20 +0100

    using the spotify api

--- music/views.py
+++ music/views.py
@@ -1,12 +1,100 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+import requests
+
+
+
+# Create your views here.
+def top_artists():
+    url = "https://spotify-scraper.p.rapidapi.com/v1/chart/artists/top"
+    querystring = {"type":"weekly"}
+    
+    headers = {
+	"x-rapidapi-key": "420f087612msh2ab2f3663ca16f9p12fd4bjsn6e69cd71ebc7",
+	"x-rapidapi-host": "spotify-scraper.p.rapidapi.com"
+}
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    response_data = response.json()
+
+    artists_info = []
+
+    if 'artists' in response_data:
+
+        for artist in response_data['artists']:
+            name = artist.get('name', 'No Name')
+            avatar_url = artist.get('visuals', {}).get('avatar', [{}])[0].get('url', 'No URL')
+            artist_id = artist.get('id', 'No ID')
+            artists_info.append((name, avatar_url, artist_id))
+
+    return artists_info
+
+def top_tracks():
+    url = "https://spotify-scraper.p.rapidapi.com/v1/chart/tracks/top"
+    
+    querystring = {"type":"weekly"}
+    
+    headers = {
+	"x-rapidapi-key": "420f087612msh2ab2f3663ca16f9p12fd4bjsn6e69cd71ebc7",
+	"x-rapidapi-host": "spotify-scraper.p.rapidapi.com"
+}
+
+    response = requests.get(url, headers=headers, params=querystring)
+    data = response.json()
+
+    track_details = []
+
+    if 'tracks' in data:
+
+        shortened_data = data['tracks'][:18]
+
+        for track in shortened_data:
+            track_id = track['id']
+            track_name = track['name']
+            artist_name = track['artists'][0]['name'] if track['artists'] else None
+            cover_url = track['album']['cover'][0]['url'] if track['album']['cover'] else None
+
+            track_details.append({
+                'id': track_id,
+                'name': track_name,
+                'artist': artist_name,
+                'cover_url': cover_url
+            })
+
+        else:
+            print("Track  not in data")
+
+        return track_details    
+
+    
+
+
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
-    return render(request, 'index.html')
+    top_tracks_list = top_tracks()
+
+    print(top_tracks_list)
+
+    first_six = top_tracks_list[:6]
+    second_six = top_tracks_list[6:12]
+    third_six = top_tracks_list[12:18]
+
+    artists_info = top_artists()
+    print(artists_info)
+
+    context = {
+        'artists_info' : artists_info,
+        'first_six': first_six,
+        'second_six': second_six,
+        'third_six' : third_six,
+    }
+    return render(request, 'index.html', context)
 
 def login(request):
     if request.method == "POST":
@@ -31,7 +119,7 @@ def signup(request):
         password2 = request.POST['password2']
 
         if password == password2:
-            if User.objects.filter(email = email).exists():
+            if User.objects.filter(email=email).exists():
                 messages.info(request, "Email Has been Taken")
                 return redirect('signup')
             elif User.objects.filter(username=username).exists():
@@ -49,7 +137,6 @@ def signup(request):
         else:
             messages.info(request, 'Password Not Matching ....')
             return redirect('signup')
-
     
     else:
         return render(request, 'signup.html')
