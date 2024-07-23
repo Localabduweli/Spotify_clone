db73e173320a0ab6ae587f63281c07fa62495a68
Abduweli Ali <149704850+Localabduweli@users.noreply.github.com>
2024-07-23 15:38:56 +0100

    csrf

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -13,24 +13,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-w5!81q&(+l)88)74vit^fox1-n&(m-^at--6_bxy-%bow3%le1'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # Set to False in production
 
 ALLOWED_HOSTS = [
     '127.0.0.1',  
     'c0ygpdq1n4.execute-api.us-west-2.amazonaws.com',  # Your deployed domain
 ]
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,9 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'spotify_clone.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,9 +74,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -104,9 +90,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -115,9 +98,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
@@ -125,21 +105,15 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
     'https://c0ygpdq1n4.execute-api.us-west-2.amazonaws.com',
 ]
 
-# CSRF and Session Cookie Settings for production
-CSRF_COOKIE_SECURE = False  # Set to True if using HTTPS in production
-SESSION_COOKIE_SECURE = False  # Set to True if using HTTPS in production
+CSRF_COOKIE_SECURE = False  # Set to True if using HTTPS
+SESSION_COOKIE_SECURE = False  # Set to True if using HTTPS
 
-# Security Settings for production
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 X_FRAME_OPTIONS = 'DENY'
