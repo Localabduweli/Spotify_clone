d089e733a05f3b8ff1e08f52f94591a8dfe61960
Abduweli Ali <149704850+Localabduweli@users.noreply.github.com>
2024-07-23 15:28:04 +0100

    changes

--- spotify_clone/settings.py
+++ spotify_clone/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -24,11 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w5!81q&(+l)88)74vit^fox1-n&(m-^at--6_bxy-%bow3%le1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  # Set to False in production
 
-ALLOWED_HOSTS = ['127.0.0.1',  
-    'c0ygpdq1n4.execute-api.us-west-2.amazonaws.com', ]
-
+ALLOWED_HOSTS = [
+    '127.0.0.1',  
+    'c0ygpdq1n4.execute-api.us-west-2.amazonaws.com',  # Your deployed domain
+]
 
 # Application definition
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'spotify_clone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],  # Corrected BASE_DIR usage
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'spotify_clone.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,13 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER' : 'root',
+        'USER': 'root',
         'PASSWORD': 'fK2d5xJKv8Kft3Ya',
-        'HOST':'postgresql-rds-rigid-jackfruit-6qrg.cnue8q8yiyy3.us-west-2.rds.amazonaws.com',
+        'HOST': 'postgresql-rds-rigid-jackfruit-6qrg.cnue8q8yiyy3.us-west-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -106,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -118,14 +115,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS =  [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
@@ -133,3 +129,20 @@ STATICFILES_DIRS =  [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CSRF Trusted Origins
+CSRF_TRUSTED_ORIGINS = [
+    'https://c0ygpdq1n4.execute-api.us-west-2.amazonaws.com',
+]
+
+# CSRF and Session Cookie Settings for production
+CSRF_COOKIE_SECURE = False  # Set to True if using HTTPS in production
+SESSION_COOKIE_SECURE = False  # Set to True if using HTTPS in production
+
+# Security Settings for production
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
